913117a833c74f8e741b15a1e9094770f08b2274
CKn00b <ckn00b@users.noreply.github.com>
2021-01-27 10:27:05 -0500

    fixes

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -1,12 +1,13 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Wishlist.Controllers
+namespace WishList.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
-            return View();
+            return View("Index");
         }
 
         public IActionResult Error()
